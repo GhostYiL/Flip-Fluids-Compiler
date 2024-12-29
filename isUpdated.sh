e5247b5c3df0b9806b95bff2f9e66df4cae0eccb
Mist <51120128+GhostYiL@users.noreply.github.com>
2024-12-29 23:09:30 +0300

    Add files via upload

---
+++ isUpdated.sh
@@ -0,0 +1,1 @@
+#!/bin/bashexit $(curl -s https://api.github.com/repos/rlguy/Blender-FLIP-Fluids/commits?path=cmake | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60*24)  | trunc)")
