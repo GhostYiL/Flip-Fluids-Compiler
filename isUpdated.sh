ad82779a64a6dbd41aba3ba28fe74c7469d5fc12
Mist <51120128+GhostYiL@users.noreply.github.com>
2024-12-29 23:09:07 +0300

    Delete isUpdated.sh

--- isUpdated.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-exit $(curl -s https://api.github.com/repos/rlguy/Blender-FLIP-Fluids/commits?path=cmake | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60*24)  | trunc)")
