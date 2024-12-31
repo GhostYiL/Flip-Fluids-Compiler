f54f8c0d213e6c1b6d1c20de8ca109e6c4c9d2ff
Mist <51120128+GhostYiL@users.noreply.github.com>
2024-12-31 04:50:29 +0300

    Update isUpdated.sh

--- isUpdated.sh
+++ isUpdated.sh
@@ -1,1 +1,2 @@
-#!/bin/bashexit $(curl -s https://api.github.com/repos/rlguy/Blender-FLIP-Fluids/commits?path=cmake | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60*24)  | trunc)")
+#!/usr/bin/env bash
+exit $(curl -s https://api.github.com/repos/rlguy/Blender-FLIP-Fluids/commits?path=cmake | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60*24)  | trunc)")
