eab5c67713e02d9e990f4c0a401bf84763e675c1
Mist <51120128+GhostYiL@users.noreply.github.com>
2025-01-03 02:13:18 +0300

    Update isUpdated.sh
    refactoring

--- isUpdated.sh
+++ isUpdated.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-exit $(curl -s https://api.github.com/repos/rlguy/Blender-FLIP-Fluids/commits?path=cmake | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60*24)  | trunc)")
+
+latest_commit_date=$(curl -s "https://api.github.com/repos/rlguy/Blender-FLIP-Fluids/commits?path=cmake" | jq -r '.[0].commit.author.date')
+
+if [ -z "$latest_commit_date" ]; then
+  echo "No commits found or unable to retrieve commit data."
+  exit 1
+fi
+
+days_since_commit=$(( ( $(date +%s) - $(date -d "$latest_commit_date" +%s) ) / (60 * 60 * 24) ))
+echo "Days since last commit: $days_since_commit"
+
+exit $days_since_commit
